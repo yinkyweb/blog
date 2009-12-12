ae5a58efaae6e47917ef45d3cfd93101da89f5b2
yinkyweb <yinkyweb@gmail.com>
2009-12-13 01:26:46 +0900

    tmp.

---
+++ htdocs/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+// $conf = parse_ini_file('../config.ini', true);
+
+// $dbh = new PDO(sprintf('mysql:host=%s;dbname=%s', $conf['db']['host'], $conf['db']['name']), $conf['db']['user'], $conf['db']['pass']);
+// $sql = "CREATE TABLE users (
+//           id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+//           user VARCHAR(255) NOT NULL,
+//           pass VARCHAR(255) NOT NULL DEFAULT ''
+//         ) DEFAULT CHARSET = utf8";
+// $sth = $dbh->prepare($sql);
+// $sth->execute();
+
+?>
---
+++ htdocs/start.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+ <title>Setting</title>
+</head>
+
+<body>
+
+<p>
+Please enter new user name and password.
+</p>
+
+<p>
+<form action="." method="post">
+<table>
+ <tr>
+  <th>name</th>
+  <td><input type="text" name="user" /></td>
+ </tr>
+ <tr>
+  <th>pass</th>
+  <td><input type="text" name="pass" /></td>
+ </tr>
+ <tr>
+  <td colspan="2"><input type="submit"></td>
+ </tr>
+</table>
+</form>
+</p>
+
+</body>
+
+</html>
